737c3329af9185a6f212e2100b15b0028977f4d7
Arthur Vigil <ahvigil@mail.sfsu.edu>
2015-05-15 15:14:29 -0700

    Final registration version

--- src/registration/Registration.cxx
+++ src/registration/Registration.cxx
@@ -1,167 +1,126 @@
 #include "DemonFire.h"
-#include <itkSimpleMultiResolutionImageRegistrationUI.h>
-#include <itkMultiResolutionImageRegistrationMethod.h>
 
-template <typename TRegistration>
-class RegistrationInterfaceCommand : public itk::Command
-{
-public:
-  typedef  RegistrationInterfaceCommand   Self;
-  typedef  itk::Command                   Superclass;
-  typedef  itk::SmartPointer<Self>        Pointer;
-  itkNewMacro( Self );
-protected:
-  RegistrationInterfaceCommand() {};
-public:
-  typedef   TRegistration                              RegistrationType;
-  typedef   RegistrationType *                         RegistrationPointer;
-  typedef   itk::RegularStepGradientDescentOptimizer   OptimizerType;
-  typedef   OptimizerType *                            OptimizerPointer;
-  void Execute(itk::Object * object, const itk::EventObject & event) ITK_OVERRIDE
-    {
-    if( !(itk::IterationEvent().CheckEvent( &event )) )
-      {
-      return;
-      }
-    RegistrationPointer registration = static_cast<RegistrationPointer>( object );
-    if(registration == ITK_NULLPTR)
-      {
-      return;
-      }
-    OptimizerPointer optimizer = static_cast< OptimizerPointer >(registration->GetModifiableOptimizer() );
-    std::cout << "-------------------------------------" << std::endl;
-    std::cout << "MultiResolution Level : "
-              << registration->GetCurrentLevel()  << std::endl;
-    std::cout << std::endl;
-    if ( registration->GetCurrentLevel() == 0 )
-      {
-      optimizer->SetMaximumStepLength( 16.00 );
-      optimizer->SetMinimumStepLength( 0.01 );
-      }
-    else
-      {
-      optimizer->SetMaximumStepLength( optimizer->GetMaximumStepLength() / 4.0 );
-      optimizer->SetMinimumStepLength( optimizer->GetMinimumStepLength() / 10.0 );
-      }
-    }
-  void Execute(const itk::Object * , const itk::EventObject & ) ITK_OVERRIDE
-    { return; }
-};
-
-
-//  The following section of code implements an observer
-//  that will monitor the evolution of the registration process.
-//
 class CommandIterationUpdate : public itk::Command
 {
 public:
-  typedef  CommandIterationUpdate   Self;
-  typedef  itk::Command             Superclass;
-  typedef  itk::SmartPointer<Self>  Pointer;
-  itkNewMacro( Self );
+    typedef CommandIterationUpdate Self;
+    typedef itk::Command Superclass;
+    typedef itk::SmartPointer<Self> Pointer;
+    typedef itk::RegularStepGradientDescentOptimizerv4<double> OptimizerType;
+    typedef const OptimizerType * OptimizerPointer;
+    itkNewMacro( Self );
 protected:
-  CommandIterationUpdate() {};
+    CommandIterationUpdate() {};
 public:
-  typedef   itk::RegularStepGradientDescentOptimizer OptimizerType;
-  typedef   const OptimizerType *                    OptimizerPointer;
-  void Execute(itk::Object *caller, const itk::EventObject & event) ITK_OVERRIDE
+    void Execute(itk::Object *caller, const itk::EventObject & event)
     {
-      Execute( (const itk::Object *)caller, event);
+        Execute( (const itk::Object *)caller, event);
     }
-  void Execute(const itk::Object * object, const itk::EventObject & event) ITK_OVERRIDE
+    
+    void Execute(const itk::Object * object, const itk::EventObject & event)
     {
-      OptimizerPointer optimizer = static_cast< OptimizerPointer >( object );
-      if( !(itk::IterationEvent().CheckEvent( &event )) )
+        OptimizerPointer optimizer =
+        static_cast< OptimizerPointer >( object );
+        if( ! itk::IterationEvent().CheckEvent( &event ) )
         {
-        return;
+            return;
         }
-      std::cout << optimizer->GetCurrentIteration() << "   ";
-      std::cout << optimizer->GetValue() << "   ";
-      std::cout << optimizer->GetCurrentPosition() << std::endl;
+        std::cout << optimizer->GetCurrentIteration() << " = ";
+        std::cout << optimizer->GetValue() << " : ";
+        std::cout << optimizer->GetCurrentPosition() << std::endl;
     }
 };
 
 namespace fire {
     // this is a function that takes in fixed and moving images
     // and performs registration on them. Returns a transform for the moving image
-    TransformType::Pointer doRegistration(ImageType::Pointer fixed, ImageType::Pointer moving){
+    TransformType::ConstPointer doRegistration(ImageType::Pointer fixed, ImageType::Pointer moving){
 
-  //  All the components are instantiated using their \code{New()} method
-  //  and connected to the registration object as in previous example.
-  //
-  TransformType::Pointer      transform     = TransformType::New();
-  OptimizerType::Pointer      optimizer     = OptimizerType::New();
-  InterpolatorType::Pointer   interpolator  = InterpolatorType::New();
-  RegistrationType::Pointer   registration  = RegistrationType::New();
-  MetricType::Pointer         metric        = MetricType::New();
-  FixedImagePyramidType::Pointer fixedImagePyramid =
-      FixedImagePyramidType::New();
-  MovingImagePyramidType::Pointer movingImagePyramid =
-      MovingImagePyramidType::New();
-  registration->SetOptimizer(     optimizer     );
-  registration->SetTransform(     transform     );
-  registration->SetInterpolator(  interpolator  );
-  registration->SetMetric( metric  );
-  registration->SetFixedImagePyramid( fixedImagePyramid );
-  registration->SetMovingImagePyramid( movingImagePyramid );
-
-  registration->SetFixedImage(    fixed    );
-  registration->SetMovingImage(   moving   );
-  
-  typedef RegistrationType::ParametersType ParametersType;
-  ParametersType initialParameters( transform->GetNumberOfParameters() );
-  initialParameters[0] = 0.0;  // Initial offset in mm along X
-  initialParameters[1] = 0.0;  // Initial offset in mm along Y
-  initialParameters[2] = 0.0;  // Initial offset in mm along Z
-  registration->SetInitialTransformParameters( initialParameters );
-  metric->SetNumberOfHistogramBins( 128 );
-  metric->SetNumberOfSpatialSamples( 50000 );
-
-  metric->ReinitializeSeed( 76926294 );
-
-  optimizer->SetNumberOfIterations( 200 );
-  optimizer->SetRelaxationFactor( 0.9 );
-  
-  // Create the Command observer and register it with the optimizer.
-  //
-  CommandIterationUpdate::Pointer observer = CommandIterationUpdate::New();
-  optimizer->AddObserver( itk::IterationEvent(), observer );
-  typedef RegistrationInterfaceCommand<RegistrationType> CommandType;
-  CommandType::Pointer command = CommandType::New();
-  registration->AddObserver( itk::IterationEvent(), command );
-  registration->SetNumberOfLevels( 3 );
-  try
-    {
-    registration->Update();
-    std::cout << "Optimizer stop condition: "
-              << registration->GetOptimizer()->GetStopConditionDescription()
-              << std::endl;
-    }
-  catch( itk::ExceptionObject & err )
-    {
-    std::cout << "ExceptionObject caught !" << std::endl;
-    std::cout << err << std::endl;
-    return NULL;
-    }
-  ParametersType finalParameters = registration->GetLastTransformParameters();
-  double TranslationAlongX = finalParameters[0];
-  double TranslationAlongY = finalParameters[1];
-  double TranslationAlongZ = finalParameters[2];
-  unsigned int numberOfIterations = optimizer->GetCurrentIteration();
-  double bestValue = optimizer->GetValue();
-  // Print out results
-  //
-  std::cout << "Result = " << std::endl;
-  std::cout << " Translation X = " << TranslationAlongX  << std::endl;
-  std::cout << " Translation Y = " << TranslationAlongY  << std::endl;
-  std::cout << " Translation Z = " << TranslationAlongZ  << std::endl;
-  std::cout << " Iterations    = " << numberOfIterations << std::endl;
-  std::cout << " Metric value  = " << bestValue          << std::endl;
+	FirstPassMetricType::Pointer metric1 = FirstPassMetricType::New();
+	SecondPassMetricType::Pointer metric2 = SecondPassMetricType::New();
+	OptimizerType::Pointer optimizer = OptimizerType::New();
+    TransformType::Pointer      transform     = TransformType::New();
+    RegistrationType::Pointer   registration  = RegistrationType::New();
+    RegistrationType::Pointer   registration_MI = RegistrationType::New();
 	
-  TransformType::Pointer finalTransform = TransformType::New();
-  finalTransform->SetParameters( finalParameters );
-  finalTransform->SetFixedParameters( transform->GetFixedParameters() );
+	registration->SetOptimizer(optimizer);
+	registration->SetMetric(metric1);       // mean squares
+	registration_MI ->SetOptimizer(optimizer);
+	registration_MI ->SetMetric(metric2);    // mutual information
   
-  return finalTransform;
+	registration->SetFixedImage(fixed);
+	registration->SetMovingImage(moving);
+	registration_MI->SetFixedImage(fixed);
+	registration_MI->SetMovingImage(moving);
+	
+	// initialize transform
+	TransformType::Pointer identityTransform = TransformType::New();
+	identityTransform->SetIdentity();
+    registration->SetMovingInitialTransform(identityTransform);
+	registration->SetFixedInitialTransform(identityTransform);
+	
+	// optimizer settings
+    optimizer->SetLearningRate(4);
+	optimizer->SetMinimumStepLength(0.001);
+	optimizer->SetRelaxationFactor(0.5);
+	optimizer->SetNumberOfIterations(200);
+	optimizer->AddObserver( itk::IterationEvent(), CommandIterationUpdate::New() );
+	
+    RegistrationType::ShrinkFactorsArrayType shrinkFactorsPerLevel;
+    shrinkFactorsPerLevel.SetSize( 2 );
+    shrinkFactorsPerLevel[0] = 32;
+    shrinkFactorsPerLevel[1] = 16;
+    RegistrationType::SmoothingSigmasArrayType smoothingSigmasPerLevel;
+    smoothingSigmasPerLevel.SetSize( 2 );
+    smoothingSigmasPerLevel[0] = 0;
+    smoothingSigmasPerLevel[1] = 0;
+    registration->SetNumberOfLevels ( 2 );
+    registration->SetShrinkFactorsPerLevel( shrinkFactorsPerLevel );
+    registration->SetSmoothingSigmasPerLevel( smoothingSigmasPerLevel );
+    
+    registration->SetMetricSamplingPercentage(1.0);
+    
+    // do the actual registration
+    registration->Update();
+
+    std::cout << "First pass registration finished with following results:" << std::endl;
+    std::cout << optimizer->GetCurrentIteration() << " = ";
+    std::cout << optimizer->GetValue() << " : ";
+    std::cout << optimizer->GetCurrentPosition() << std::endl;
+    
+    registration_MI->SetMetric(metric2);
+    // initialize with results of mean squares registration
+    registration_MI->SetMovingInitialTransform(registration->GetTransform());
+    
+    // set shrink factors for up to six levels
+    shrinkFactorsPerLevel.SetSize( 6 );
+    shrinkFactorsPerLevel[0] = 32;
+    shrinkFactorsPerLevel[1] = 16;
+    shrinkFactorsPerLevel[2] = 8;
+    shrinkFactorsPerLevel[3] = 4;
+    shrinkFactorsPerLevel[4] = 2;
+    shrinkFactorsPerLevel[5] = 1;
+    
+    // for now don't do any smoothing
+    smoothingSigmasPerLevel.SetSize( 6 );
+    smoothingSigmasPerLevel[0] = 0;
+    smoothingSigmasPerLevel[1] = 0;
+    smoothingSigmasPerLevel[2] = 0;
+    smoothingSigmasPerLevel[3] = 0;
+    smoothingSigmasPerLevel[4] = 0;
+    smoothingSigmasPerLevel[5] = 0;
+    
+    // increase number of levels for higher resolution registration
+    registration_MI->SetNumberOfLevels ( 2 );
+    registration_MI->SetShrinkFactorsPerLevel( shrinkFactorsPerLevel );
+    registration_MI->SetSmoothingSigmasPerLevel( smoothingSigmasPerLevel );
+    
+    registration_MI->Update();
+    
+    std::cout << "Registration finished with following results:" << std::endl;
+    std::cout << optimizer->GetCurrentIteration() << " = ";
+    std::cout << optimizer->GetValue() << " : ";
+    std::cout << optimizer->GetCurrentPosition() << std::endl;
+	return registration_MI->GetTransform();
     }
 }
